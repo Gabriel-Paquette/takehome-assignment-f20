0868a7c4ada8a4c29a1d0879416efe5eba4d5b1a
Gabriel-Paquette <61212491+Gabriel-Paquette@users.noreply.github.com>
2020-09-24 23:58:37 -0400

    Add files via upload

---
+++ Counter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+   <p>I'm a Counter!</p>
+   <br />
+   <p>Current count is {{ count }} </p>
+   <button v-on:click = "count += 1">Increase</button>
+   <button v-on:click = "count -= 1">Decrease</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    };
+  }
+};
+</script>
+
+<style>
+</style>
+
---
+++ Show.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- PART 4: Add your code here -->
+  <div>
+      <p> hello {{name}} </p>
+  </div>
+</template>
+
+<script>
+// PART 4: Add your code here
+export default {
+    id: Number,
+    name: String,
+    episodes_seen: Number
+};
+</script>
+
+<style>
+</style>
